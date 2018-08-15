32e2504359348886e1a3f2fa673d6b40f3986416
jiangfeng <jiangfeng@zentech-inc.com>
2018-08-15 16:00:01 +0800

    modify views second

--- MyEye/audit/views.py
+++ MyEye/audit/views.py
@@ -325,7 +325,7 @@ def rule_operation(request):
 
 @auth
 def rule_del(request):
-    #删除rule
+    #删除rule规则
     if request.method == 'POST':
         data = {'status': True, 'error': "", 'message': ''}
         rule_id = request.POST.get('rule_id')
